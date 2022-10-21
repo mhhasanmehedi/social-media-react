5616eaf13db609eb1434c6ed52ec0e4cdfd73e73
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-21 22:40:28 +0600

    installation

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AccessAlarm } from "@mui/icons-material";
+
+const Home = () => {
+  return (
+    <div>
+      <AccessAlarm />
+    </div>
+  );
+};
+
+export default Home;
